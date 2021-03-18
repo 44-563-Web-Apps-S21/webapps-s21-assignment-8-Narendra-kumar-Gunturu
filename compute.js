efe065b1f6405c632997df63eefd5b10e2bdc65e
Narendra-kumar-Gunturu <77824814+Narendra-kumar-Gunturu@users.noreply.github.com>
2021-03-17 21:34:03 -0500

    fetch.html

--- compute.js
+++ compute.js
@@ -24,7 +24,7 @@ const server =
           // Process the queries here
           res.statusCode = 200      //code for OK
           res.setHeader('Content-Type', 'text/plain')
-          res.write(`Cube root of ${x} is ${Math.ceil(x)}`)
+          res.write(`Ceil of ${x} is ${Math.ceil(x)}`)
           res.end();
         
       } else {
